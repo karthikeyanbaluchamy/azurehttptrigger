3eba37c1ec96c88cadc90bc0a5c33f7e19280171
navikarthi <navikarthius@gmail.com>
2024-10-17 11:50:12 -0400

    new changes

---
+++ src/main/java/org/expos/functions/HttpTriggerJava.java
@@ -0,0 +1,38 @@
+package org.expos.functions;
+
+import java.util.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class HttpTriggerJava {
+    /**
+     * This function listens at endpoint "/api/HttpTriggerJava". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpTriggerJava
+     * 2. curl {your host}/api/HttpTriggerJava?name=HTTP%20Query
+     */
+    @FunctionName("Multiply")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
+       int v1= Integer.parseInt(request.getQueryParameters().get("num1"));
+       int v2= Integer.parseInt(request.getQueryParameters().get("num2"));
+       Map<String,Integer> hm = new HashMap<>();
+       hm.put("The result of Multiplication is",v1*v2);
+
+
+
+        if (hm == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body(hm).build();
+        }
+    }
+}
